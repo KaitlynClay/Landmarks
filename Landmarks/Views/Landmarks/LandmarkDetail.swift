75858e5a86910d4d24d9f86ac5edd31c237a0926
Kaitlyn Clay <kclay@students.mchenry.edu>
2024-03-21 18:27:53 -0500

    in progress

--- Landmarks/Views/Landmarks/LandmarkDetail.swift
+++ Landmarks/Views/Landmarks/LandmarkDetail.swift
@@ -25,8 +25,9 @@ struct LandmarkDetail: View {
                 HStack {
                     Text(landmark.name)
                         .font(.title)
-                    FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                    FavoriteButton(isSet: modelData.landmarks[landmarkIndex].isFavorite)
                 }
+                
                 HStack {
                     Text(landmark.park)
                         .font(.subheadline)
